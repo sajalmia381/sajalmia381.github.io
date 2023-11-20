8ff0dd2afa34415da58e919e2c815e0ac544fb20
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-21 00:02:46 +0600

    feat: anchor scroll add

---
+++ src/app/home/components/toolbar/toolbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mia-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}

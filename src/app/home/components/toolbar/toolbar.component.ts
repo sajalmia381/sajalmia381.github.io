97bde1a454381286444e0eee6302d1e0716802ae
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-21 23:16:27 +0600

    animatiion init

--- src/app/home/components/toolbar/toolbar.component.ts
+++ src/app/home/components/toolbar/toolbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { fadeInUpOnEnterAnimation } from '@shared/animations';
 
 @Component({
   selector: 'mia-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
+  animations: [
+    fadeInUpOnEnterAnimation({anchor: 'fadeInUp'}),
+  ]
 })
 export class ToolbarComponent {
 
